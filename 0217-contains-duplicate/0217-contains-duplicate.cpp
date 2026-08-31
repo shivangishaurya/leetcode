64db34df7e7c737f6b7f339c2d259cd036af0328
shivangishaurya <shivangiishauryaa@gmail.com>
2026-08-31 19:08:16 +0530

    Time: 67 ms (71.53%), Space: 111.4 MB (28.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> map;
         for (int val : nums) {
-            if(map.find(val)!=map.end())return true;
-            else map[val]++;
+            map[val]++;
+            auto it=map.find(val);
+            if(it!=map.end() && it->second>1)
+            return true;
         }
         return false;
     }
