93dd455d59f97a736f9fd952453e75a536623ae9
shivangishaurya <shivangiishauryaa@gmail.com>
2026-08-31 22:54:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.33%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        unordered_map<char,int>map;
-        for(char c:s){
-            map[c]++;
-        }
-        for(char a:t){
-            if(map[a]==0)
-            return a;
-            map[a]--;
-        }
-        return 'y';
+       int ans=0;
+       for(char c:s){
+        ans^=c;
+       }
+       for(char a:t){
+        ans^=a;
+       }
+       return ans;
     }
 };
