ad54196419f18715d7c29f5e62382d6212fa5fae
shivangishaurya <shivangiishauryaa@gmail.com>
2026-09-07 11:46:02 +0530

    Time: 0 ms (100%), Space: 31.3 MB (29.45%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -3,20 +3,27 @@ public:
     int firstStableIndex(vector<int>& nums, int k) 
     {
         int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            int min=INT_MAX;
-            int max=INT_MIN;
-            for(int j=0;j<=i;j++){
-                if(nums[j]>max){
-                    max=nums[j];
-                }
+        vector<int>maxarray;
+        vector<int>minarray;
+        int max=nums[0];
+        maxarray.push_back(max);
+        for(int i=1;i<n;i++){
+            if(nums[i]>max){
+                max=nums[i];
             }
-            for(int j=i;j<=n-1;j++){
-                if(nums[j]<min){
-                    min=nums[j];
-                }
+            maxarray.push_back(max);
+        }
+        int min=nums[n-1];
+        minarray.push_back(min);
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<min){
+                min=nums[i];
             }
-            if(max-min<=k)
+            minarray.push_back(min);
+        }
+        reverse(minarray.begin(),minarray.end());
+        for(int i=0;i<n;i++){
+            if(maxarray[i]-minarray[i]<=k)
             return i;
         }
         return -1;
