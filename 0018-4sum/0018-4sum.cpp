f2192fd1ad7279e1ea0ac491f6eb01341130b475
shivangishaurya <shivangiishauryaa@gmail.com>
2026-09-18 14:59:46 +0530

    Time: 24 ms (34.03%), Space: 17.4 MB (87.31%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -13,7 +13,7 @@ public:
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for (int j = i + 1; j < n; ) {
+            for (int j = i + 1; j < n;) {
 
                 int p = j + 1, q = n - 1;
 
